a8f3eb323fe183e4cc47c110e35082f7ac480c26
Cindy <cindy.c0106@gmail.com>
2022-08-31 19:48:16 +0000

    adding client.js

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost", //IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: CC")
+  });
+
+  
+  conn.on("data", () => {
+    console.log('you ded cuz you idled');
+  });
+
+  return conn;
+};
+
+module.exports = connect;
